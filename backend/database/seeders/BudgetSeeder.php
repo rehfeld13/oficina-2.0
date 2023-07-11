285b9038bfda5d8a427654e496a24cb35206c269
Rafael <rafaelrehfeld97@gmail.com>
2023-07-11 20:41:48 -0300

    Adding BudgetSeeder to Populate the Database

---
+++ backend/database/seeders/BudgetSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Budget;
+
+class BudgetSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        for ($i = 0; $i < 5; $i++) {
+            Budget::create([
+                'nameClient' => 'Cliente ' . ($i + 1),
+                'nameSeller' => 'Vendedor ' . ($i + 1),
+                'description' => 'Descrição do orçamento ' . ($i + 1),
+                'value' => 1000 + ($i * 100),
+                'dateAndTime' => now(),
+            ]);
+    }
+  }
+}
